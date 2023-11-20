f003d1ca9049953c758abddba3a3226f963087f6
vash456 <darlin.estrada456@gmail.com>
2023-11-19 23:23:30 -0500

    docker y base de datos

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ git init
 npm init -y
 npm install express --save
 npm install nodemon
+npm install pg //postgresql
+
+Docker
+
+docker-compose up --build
+docker-compose down
---
+++ scripts/tasks.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS "TASKS";
+
+CREATE TABLE "TASKS"(
+    id serial primary key NOT NULL,
+    task character varying(20) NOT NULL,
+    detail character varying(200) NULL,
+    state int default 1,
+    date date,
+    time time
+);
+
+INSERT INTO "TASKS"(TASK, DETAIL, STATE, DATE, TIME)
+VALUES
+    ('tarea 2', 'Es el tarea 2', 1, '2023/11/01', '13:00:00'),
+    ('tarea 1', 'Es el tarea 1', 1, '2023/11/01', '13:00:00'),
+    ('tarea 3', 'Es el tarea 3', 1, '2023/11/01', '13:00:00'),
+    ('tarea 4', 'Es el tarea 4', 1, '2023/11/01', '13:00:00'),
+    ('tarea 5', 'Es el tarea 5', 1, '2023/11/01', '13:00:00'),
+    ('tarea 6', 'Es el tarea 6', 1, '2023/11/01', '13:00:00');
+
+SELECT * FROM "TASKS"
